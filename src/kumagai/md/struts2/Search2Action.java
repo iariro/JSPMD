e965cca461c8e803340eba969acd2f439a0aa291
kumagai <iariro@gol.com>
2025-02-26 21:35:08 +0900

    数字ソート

--- src/kumagai/md/struts2/Search2Action.java
+++ src/kumagai/md/struts2/Search2Action.java
@@ -1,12 +1,24 @@
 package kumagai.md.struts2;
 
-import java.sql.*;
-import java.util.*;
-import javax.servlet.*;
-import com.microsoft.sqlserver.jdbc.*;
-import org.apache.struts2.*;
-import org.apache.struts2.convention.annotation.*;
-import kumagai.md.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import javax.servlet.ServletContext;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+import kumagai.md.DiskAndRecordCore;
 
 /**
  * 検索アクション。
@@ -16,6 +28,21 @@ import kumagai.md.*;
 @Result(name="success", location="/md/search2.jsp")
 public class Search2Action
 {
+	public static void main(String[] args) throws Exception
+	{
+		DriverManager.registerDriver(new SQLServerDriver());
+
+		Connection connection =
+			DriverManager.getConnection
+				("jdbc:sqlserver://localhost:2144;DatabaseName=MD;User=sa;Password=p@ssw0rd;");
+
+		ArrayList<DiskAndRecordCore> diskAndRecords = executeCore("", "83", "1", connection);
+		for (DiskAndRecordCore diskAndRecord : diskAndRecords)
+		{
+			System.out.printf("%s %s\n", diskAndRecord.getName(), diskAndRecord.getTitle());
+		}
+	}
+
 	public String composerId;
 	public String title;
 	public String symphony;
@@ -38,6 +65,13 @@ public class Search2Action
 			DriverManager.getConnection
 				(context.getInitParameter("MDSqlserverUrl"));
 
+		diskAndRecords = executeCore(title, composerId, symphony, connection);
+
+		return "success";
+	}
+
+	static ArrayList<DiskAndRecordCore> executeCore(String title, String composerId, String symphony, Connection connection) throws SQLException
+	{
 		String sql =
 			"select recordid, diskid, name, title, date, memo from record join composer on record.composerid=composer.composerid where record.composerid=? and symphony=?";
 
@@ -64,7 +98,7 @@ public class Search2Action
 
 		ResultSet results = statement.executeQuery();
 
-		diskAndRecords = new ArrayList<DiskAndRecordCore>();
+		ArrayList<DiskAndRecordCore> diskAndRecords = new ArrayList<DiskAndRecordCore>();
 
 		while (results.next())
 		{
@@ -75,6 +109,26 @@ public class Search2Action
 		statement.close();
 		connection.close();
 
-		return "success";
+		Collections.sort(diskAndRecords,
+			new Comparator<DiskAndRecordCore>()
+			{
+				public int compare(DiskAndRecordCore o1, DiskAndRecordCore o2)
+				{
+					try
+					{
+						Integer no1 = Integer.parseInt(o1.getTitle());
+						Integer no2 = Integer.parseInt(o2.getTitle());
+						return no1.compareTo(no2);
+					}
+					catch (NumberFormatException exception)
+					{
+						String no1 = o1.getTitle();
+						String no2 = o2.getTitle();
+						return no1.compareTo(no2);
+					}
+				}
+			});
+
+		return diskAndRecords;
 	}
 }
